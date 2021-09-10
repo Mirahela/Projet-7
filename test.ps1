be122bc268a35466d6c6fa0f13ee6e81695ebca5
Mirahela <90244382+Mirahela@users.noreply.github.com>
2021-09-10 11:59:29 +0200

    test de commit

---
+++ test.ps1
@@ -0,0 +1,16 @@
+﻿ 
+ Param(
+     $user= ""
+ )
+
+ if( $user -eq ""){
+
+        $user = read-host "Qui est l'utilisateur ?"
+ 
+ }
+
+   Get-ADuser -Identity $user -properties memberof | Select-Object memberof |Format-List
+
+ 
+
+  exit 1
