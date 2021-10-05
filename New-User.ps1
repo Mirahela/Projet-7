2f4430898c1a5860db7f305f6588fdcabfdbe48a
Mirahela <90244382+Mirahela@users.noreply.github.com>
2021-10-05 19:01:17 +0200

    Ver2

--- New-User.ps1
+++ New-User.ps1
@@ -1,5 +1,6 @@
 ﻿#Creer un nouvel utilisateur, creer son dosier personnelle et partagé celui-ci.
 
+$erroractionreference = "stop"
 
 $Name = Read-host "Quel est le prénom et le nom de l'utilisateur"
 
@@ -13,9 +14,20 @@ New-Item -path "c:\DP\" -name $Samaccountname -Type Directory
 
 New-Smbshare -Name $SamAccountName -FullAccess $SamAccountName 
 
-if (@(Get-ADUser -Filter {SamAccountName -eq ""}).count -eq 0)
+#try { $ADUser = Get-ADuser -Identity $name -erroraction stop }
 
-{ Write-Warning -Message "L'utilisateur n'existe pas."}
+#catch 
+#{Write-Warning "L'utilisateur existe déjà" 
+ #      break
+  #  }
+
+  
+$existe = get-ADUser -Filter "SamAccountName -eq '$Username'"
+
+if ($existe -eq $null){Write-host "L'utilisateur n'existe pas" }
+else{stop}
+ 
+
 
 
 Exit 1
